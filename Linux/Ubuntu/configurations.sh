2f20036804ad7fc45d79d703f703a9d84bd4e9e9
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-19 10:27:36 +0300

    Create configurations.sh in ubuntu and added  aws configure

---
+++ Linux/Ubuntu/configurations.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#######################################################################
+#Script Name	  :configurations.sh                                             
+#Description	  :Provides a UI to navigate through the program                                 
+#Author       	  :Derrick Shibero
+#Email         	  :shiberoderrickwakhu@gmail.com                    
+#Date         	  :Fri Jul  1 17:15:50 EAT 2024                          
+#Version          :fork 1.0.0
+#######################################################################
+
+aws_configurations(){
+  echo "aws configuration"
+  aws configure
+}
